7c94f9cb1084bf630f6c27f65af4bd5adffbed9a
Samir Boufatah <samir.boufatah@gmail.com>
2020-09-22 12:08:37 +0200

    Autoformat Java files

--- src/main/java/com/sample/app/MainActivity.java
+++ src/main/java/com/sample/app/MainActivity.java
@@ -4,39 +4,43 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.snackbar.Snackbar;
 import com.sample.lib.ktgenerator.KtIntGenerator;
 
 class MainActivity extends AppCompatActivity {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_main);
-        setSupportActionBar(findViewById(R.id.toolbar));
+  @Override
+  public void onCreate(
+      @Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    super.onCreate(savedInstanceState, persistentState);
+    setContentView(R.layout.activity_main);
+    setSupportActionBar(findViewById(R.id.toolbar));
 
-        findViewById(R.id.fab).setOnClickListener(view ->
-                Snackbar.make(view, "Your lucky number: " + KtIntGenerator.next(0, 100), Snackbar.LENGTH_LONG).show()
-        );
-    }
+    findViewById(R.id.fab)
+        .setOnClickListener(
+            view ->
+                Snackbar.make(
+                        view,
+                        "Your lucky number: " + KtIntGenerator.next(0, 100),
+                        Snackbar.LENGTH_LONG)
+                    .show());
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    if (item.getItemId() == R.id.action_settings) {
+      return true;
     }
+    return super.onOptionsItemSelected(item);
+  }
 }
--- src/main/java/com/sample/app/SecondFragment.java
+++ src/main/java/com/sample/app/SecondFragment.java
@@ -1,34 +1,35 @@
 package com.sample.app;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
+/** A simple [Fragment] subclass as the second destination in the navigation. */
 class SecondFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
-    }
+  @Nullable
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_second, container, false);
+  }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(v ->
-                NavHostFragment.findNavController(this).navigate(R.id.action_SecondFragment_to_FirstFragment));
-    }
+    view.findViewById(R.id.button_second)
+        .setOnClickListener(
+            v ->
+                NavHostFragment.findNavController(this)
+                    .navigate(R.id.action_SecondFragment_to_FirstFragment));
+  }
 }
